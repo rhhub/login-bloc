8df3b0d330aeb4896320c2884496ed914bbf9cde
Ryan Huebert <7444613+rhhub@users.noreply.github.com>
2019-05-20 20:07:42 -0700

    Finish Section 15 Consuming Bloc Data

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: <Widget>[
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25)),
           submitButton(),
         ],
@@ -18,7 +21,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder<Object>(
       stream: bloc.email,
       builder: (context, snapshot) {
@@ -35,7 +38,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder<Object>(
       stream: bloc.password,
       builder: (context, snapshot) {
