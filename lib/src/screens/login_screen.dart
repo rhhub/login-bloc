5bd7e1e4f54e9c4cca8243316e960891ed277f70
Ryan Huebert <7444613+rhhub@users.noreply.github.com>
2019-05-20 20:53:30 -0700

    Finish Section 16 RxDart

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -15,7 +15,7 @@ class LoginScreen extends StatelessWidget {
           emailField(bloc),
           passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25)),
-          submitButton(),
+          submitButton(bloc),
         ],
       ),
     );
@@ -54,11 +54,18 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      child: Text('Login'),
-      color: Colors.blue,
-      onPressed: () {},
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder<Object>(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue,
+          onPressed: snapshot.hasData
+              ? bloc.submit
+              : null,
+        );
+      },
     );
   }
 }
